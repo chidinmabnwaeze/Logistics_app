a94a541989d06c9a896d12c83b520e1b39a24909
unknown <chidinmabnwaeze@gmail.com>
2024-05-11 07:26:21 +0100

    update

--- new-project/src/components/main.js
+++ new-project/src/components/main.js
@@ -3,7 +3,25 @@ import react from "react";
 function main (){
     return (
         <div>
-           
+           <section className="section1">
+
+            <div className="middle-text">
+            <h2 className="center-text">The one-stop hub for everything logistics</h2>
+            <p className="sub-text">Best prices. Seamless experience. Multiple service option</p>
+            <button className="delivery-request">Send Delivery Request Now</button>
+            </div>
+           </section>
+
+           /* the faint logos section */
+`           <section className="faint-logo">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />`
+           </section>
+
+           <section className="logistics">
+
+           </section>
         </div>
     )
 };
